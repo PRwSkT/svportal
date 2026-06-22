ad75b60c07af3d8721ff4bf5ba31bb0747fd8311
Nut <nut.svg@gmail.com>
2026-06-22 13:39:47 +0700

    chore: remove test file

--- test-thai.ts
+++
@@ -1,8 +0,0 @@
-import * as thai from 'thai-address-universal';
-async function test() {
-  console.log('Districts in กรุงเทพมหานคร:', (await thai.getDistrictByProvince('กรุงเทพมหานคร')).length);
-  console.log('SubDistricts in บางรัก:', (await thai.getSubDistrictByDistrict('บางรัก')).length);
-  // Wait, does getSubDistrictByDistrict take 1 or 2 arguments?
-  console.log('SubDistricts in บางรัก (2 args?):', (await thai.getSubDistrictByDistrict('กรุงเทพมหานคร', 'บางรัก')).length);
-}
-test();
